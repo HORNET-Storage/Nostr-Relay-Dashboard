aa991c157df9007adc209b56d4b5bb843803e40d
Maphikza <stapisi@protonmail.com>
2024-07-06 19:12:15 +0200

    Fixing pwa installtion

---
+++ src/components/PwaSupportChecker.tsx
@@ -0,0 +1,24 @@
+// components/PwaSupportChecker.tsx
+import React, { useEffect } from 'react';
+import { useAppDispatch } from '@app/hooks/reduxHooks';
+import { setPWASupported } from '@app/store/slices/pwaSlice';
+
+const PwaSupportChecker: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const checkPWASupport = () => {
+      const isSupported = ('serviceWorker' in navigator) && ('BeforeInstallPromptEvent' in window);
+      console.log('PWA support check:', isSupported);
+      dispatch(setPWASupported(isSupported));
+    };
+
+    checkPWASupport();
+  }, [dispatch]);
+
+  return null; // This component doesn't render anything
+};
+
+export default PwaSupportChecker;
+
+
