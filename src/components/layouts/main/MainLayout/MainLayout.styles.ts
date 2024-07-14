809de8bccd2c3f107a21a055634cc65f0761e3da
TONE-E <90010906+AnthonyMarin@users.noreply.github.com>
2024-07-14 08:27:40 -0700

    fix: height issues

--- src/components/layouts/main/MainLayout/MainLayout.styles.ts
+++ src/components/layouts/main/MainLayout/MainLayout.styles.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 import { media } from '@app/styles/themes/constants';
 import { BaseLayout } from '@app/components/common/BaseLayout/BaseLayout';
 
-export const LayoutMaster = styled(BaseLayout)``;
+export const LayoutMaster = styled(BaseLayout)`
+height: 100vh;
+`;
 
 export const LayoutMain = styled(BaseLayout)`
   height: fit-content;
