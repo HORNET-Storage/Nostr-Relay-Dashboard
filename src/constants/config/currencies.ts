d7540e76c91f1cb668b006b972789eb12a3ed541
TONE-E <90010906+AnthonyMarin@users.noreply.github.com>
2024-07-16 22:18:19 -0700

    add currency symbols

--- src/constants/config/currencies.ts
+++ src/constants/config/currencies.ts
@@ -13,4 +13,41 @@ export const currencies = {
     text: 'ETH',
     icon: 'Ξ',
   },
+  [CurrencyTypeEnum.EUR]: {
+    text: 'EUR',
+    icon: '€',
+  },
+
+  [CurrencyTypeEnum.GBP]: {
+    text: 'GBP',
+    icon: '£',
+  },
+  [CurrencyTypeEnum.JPY]: {
+    text: 'JPY',
+    icon: '¥',
+  },
+  [CurrencyTypeEnum.AUD]: {
+    text: 'AUD',
+    icon: 'A$',
+  },
+  [CurrencyTypeEnum.CAD]: {
+    text: 'CAD',
+    icon: 'C$',
+  },
+  [CurrencyTypeEnum.CHF]: {
+    text: 'CHF',
+    icon: 'Fr.',
+  },
+  [CurrencyTypeEnum.CNY]: {
+    text: 'CNY',
+    icon: '¥',
+  },
+  [CurrencyTypeEnum.SEK]: {
+    text: 'SEK',
+    icon: 'kr',
+  },
+  [CurrencyTypeEnum.NZD]: {
+    text: 'NZD',
+    icon: 'NZ$',
+  },
 };
