cc3996401d35846ccc4028d036a3877cde72d1fc
TONE-E <90010906+AnthonyMarin@users.noreply.github.com>
2024-07-17 20:11:29 -0700

    SQUASH

--- src/components/nft-dashboard/Balance/components/CurrencySelect/CurrencySelect.tsx
+++ src/components/nft-dashboard/Balance/components/CurrencySelect/CurrencySelect.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '@app/hooks/reduxHooks';
 import { CurrencyTypeEnum } from '@app/interfaces/interfaces';
 import { BaseSelect, Option } from '@app/components/common/selects/BaseSelect/BaseSelect';
+import { setCurrency } from '@app/store/slices/currencySlice';
 interface CurrencySelectProps {
   currencies: CurrencyTypeEnum[];
 }
 
 const CurrencySelect: React.FC<CurrencySelectProps> = ({ currencies }) => {
-  const [selectedCurrency, setSelectedCurrency] = useState<CurrencyTypeEnum>('USD' as CurrencyTypeEnum);
+  const currency = useAppSelector((state) => state.currency.currency);
+  const dispatch = useAppDispatch();
+
   const handleChange = (value: CurrencyTypeEnum) => {
-    setSelectedCurrency(value);
+    dispatch(setCurrency(value));
   };
 
   return (
-    <BaseSelect value={selectedCurrency} onChange={(value) => handleChange(value as CurrencyTypeEnum)}>
+    <BaseSelect value={currency} onChange={(value) => handleChange(value as CurrencyTypeEnum)}>
       {currencies.map((currency) => (
         <Option key={currency} value={currency}>
           {currency}
