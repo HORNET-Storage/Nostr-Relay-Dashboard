da2b1d25833e57fa290d1d7cc844838a618fc4ef
TONE-E <90010906+AnthonyMarin@users.noreply.github.com>
2024-07-19 20:08:53 -0700

    fix: side menu not updating on logo click

--- src/components/layouts/main/sider/sidebarNavigation.tsx
+++ src/components/layouts/main/sider/sidebarNavigation.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { DashboardOutlined, TableOutlined } from '@ant-design/icons';
 import { ReactComponent as NestIcon } from '@app/assets/icons/hive.svg';
 import { ReactComponent as BtcIcon } from '@app/assets/icons/btc.svg';
 import { ReactComponent as StatsIcon } from '@app/assets/icons/stats.svg';
 import { ReactComponent as StorageSettingsIcon } from '@app/assets/icons/storage-settings.svg';
 import { useResponsive } from '@app/hooks/useResponsive';
-import { table } from 'console';
 
 export interface SidebarNavigationItem {
   title: string;
@@ -18,34 +17,37 @@ export interface SidebarNavigationItem {
 export const useSidebarNavigation = (): SidebarNavigationItem[] => {
   const { isDesktop } = useResponsive();
 
-  const items: SidebarNavigationItem[] = [
-    {
-      title: 'common.nft-dashboard',
-      key: 'nft-dashboard',
-      url: '/',
-      icon: <NestIcon />,
-    },
-    {
-      title: 'common.relay-settings',
-      key: 'storage-settings',
-      url: '/storage-settings',
-      icon: <StorageSettingsIcon />,
-    },
-    {
-      title: 'Nostr Statistics',
-      key: 'dataTables',
-      url: '/nostr-stats',
-      icon: <StatsIcon />,
-    },
-  ];
+  return useMemo(() => {
+    const items: SidebarNavigationItem[] = [
+      {
+        title: 'common.nft-dashboard',
+        key: 'nft-dashboard',
+        url: '/',
+        icon: <NestIcon />,
+      },
+      {
+        title: 'common.relay-settings',
+        key: 'storage-settings',
+        url: '/storage-settings',
+        icon: <StorageSettingsIcon />,
+      },
+      {
+        title: 'Nostr Statistics',
+        key: 'dataTables',
+        url: '/nostr-stats',
+        icon: <StatsIcon />,
+      },
+    ];
 
-  if (!isDesktop) {
-    items.push({
-      title: 'common.wallet',
-      key: 'wallet',
-      url: '/wallet',
-      icon: <BtcIcon />,
-    });
-  }
-  return items;
+    if (!isDesktop) {
+      items.push({
+        title: 'common.wallet',
+        key: 'wallet',
+        url: '/wallet',
+        icon: <BtcIcon />,
+      });
+    }
+
+    return items;
+  }, [isDesktop]);
 };
