4ba8e52902e31adbb1b49a507346ece1f2f57aed
TONE-E <90010906+AnthonyMarin@users.noreply.github.com>
2024-07-27 14:17:35 -0700

    updated labels

--- src/pages/RelaySettingsPage.tsx
+++ src/pages/RelaySettingsPage.tsx
@@ -69,31 +69,31 @@ const RelaySettingsPage: React.FC = () => {
   const { isDesktop } = useResponsive();
 
   const categories = [
-    { id: 1, name: 'Basic Features' },
-    { id: 2, name: 'Extra Features' },
-    { id: 3, name: 'Git Features' },
+    { id: 1, name: 'Basic Nostr Features' },
+    { id: 2, name: 'Extra Nostr Features' },
+    { id: 3, name: 'GitNestr Features' },
   ];
 
   const noteOptions = [
     { kind: 0, kindString: 'kind0', description: 'Metadata', category: 1 },
     { kind: 1, kindString: 'kind1', description: 'Text Note', category: 1 },
     { kind: 2, kindString: 'kind2', description: 'Recommend Relay', category: 1 },
-    { kind: 3, kindString: 'kind3', description: 'Contacts', category: 1 },
+    { kind: 3, kindString: 'kind3', description: 'Follow List', category: 1 },
     { kind: 5, kindString: 'kind5', description: 'Event Deletion', category: 1 },
     { kind: 6, kindString: 'kind6', description: 'Repost', category: 1 },
     { kind: 7, kindString: 'kind7', description: 'Reaction', category: 1 },
     { kind: 8, kindString: 'kind8', description: 'Badge Award', category: 2 },
     { kind: 16, kindString: 'kind16', description: 'Generic Repost', category: 1 },
     { kind: 10000, kindString: 'kind10000', description: 'Mute List', category: 1 },
-    { kind: 10001, kindString: 'kind10001', description: 'Pin List', category: 1 },
-    { kind: 10002, kindString: 'kind10002', description: 'Relay List Metadata', category: 1 },
+    { kind: 10001, kindString: 'kind10001', description: 'Pinned Note', category: 1 },
+    { kind: 10002, kindString: 'kind10002', description: 'Tiny Relay List', category: 1 },
     { kind: 1984, kindString: 'kind1984', description: 'Reporting', category: 1 },
-    { kind: 30000, kindString: 'kind30000', description: 'Follow List', category: 1 },
+    { kind: 30000, kindString: 'kind30000', description: 'Custom Follow List', category: 1 },
     { kind: 30008, kindString: 'kind30008', description: 'Profile Badge', category: 2 },
     { kind: 30009, kindString: 'kind30009', description: 'Badge Definition', category: 2 },
-    { kind: 30023, kindString: 'kind30023', description: 'Long-Form Content', category: 1 },
+    { kind: 30023, kindString: 'kind30023', description: 'Formatted Articles', category: 1 },
     { kind: 30079, kindString: 'kind30079', description: 'Event Paths', category: 1 },
-    { kind: 9734, kindString: 'kind9734', description: 'Lightning Zap Request', category: 2 },
+    //{ kind: 9734, kindString: 'kind9734', description: 'Lightning Zap Request', category: 2 },
     { kind: 9735, kindString: 'kind9735', description: 'Lightning Zap Invoice Receipts', category: 2 },
     { kind: 10011, kindString: 'kind10011', description: 'Issue Notes', category: 3 },
     { kind: 10022, kindString: 'kind10022', description: 'PR Notes', category: 3 },
@@ -553,7 +553,7 @@ const RelaySettingsPage: React.FC = () => {
         <BaseCol xs={24}>
           <Collapse style={{ padding: '1rem 0 1rem 0', margin: '0 0 1rem 0' }} bordered={false}>
             <StyledPanel
-              header={settings.mode !== 'smart' ? `Blacklisted Nostr Kind Numbers` : 'Nostr Kind Numbers'}
+              header={settings.mode !== 'smart' ? `Blacklisted Kind Numbers` : 'Kind Numbers'}
               key="notes"
               className="centered-header"
             >
@@ -950,7 +950,7 @@ const RelaySettingsPage: React.FC = () => {
 
         <Collapse style={{ padding: '1rem 0 1rem 0', margin: '0 0 1rem 0' }} bordered={false}>
           <StyledPanel
-            header={settings.mode !== 'smart' ? `Blacklisted Nostr Kind Numbers` : 'Nostr Kind Numbers'}
+            header={settings.mode !== 'smart' ? `Blacklisted Kind Numbers` : 'Kind Numbers'}
             key="notes"
             className="centered-header"
           >
