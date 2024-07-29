cc575deb8ea5b0e967b3e222e4b15e1388fe8baf
TONE-E <90010906+AnthonyMarin@users.noreply.github.com>
2024-07-29 16:48:03 -0700

    fix rerendering

--- src/components/nft-dashboard/activityStory/ActivityStoryItem/ActivityStoryItem.tsx
+++ src/components/nft-dashboard/activityStory/ActivityStoryItem/ActivityStoryItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { useTranslation } from 'react-i18next';
 import { WalletTransaction } from '@app/api/activity.api';
 import { Dates } from '@app/constants/Dates';
@@ -7,21 +7,28 @@ import { CurrencyTypeEnum } from '@app/interfaces/interfaces';
 import * as S from './ActivityStoryItem.styles';
 import { BaseRow } from '@app/components/common/BaseRow/BaseRow';
 import { BaseCol } from '@app/components/common/BaseCol/BaseCol';
+function makeHexId(length: number): string {
+  const characters = 'abcdef0123456789';
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
 
 export const ActivityStoryItem: React.FC<WalletTransaction> = ({ witness_tx_id, date, output, value }) => {
   const { t } = useTranslation();
+  const [transactionId, setTransactionId] = useState<string | null>(null);
 
-  // Convert value to number
-  const numericValue = parseFloat(value);
-  //function for dummy data until winness_tx_id is available !!! TODO: remove this function when winness_tx_id is available
-  function makeHexId(length: number): string {
-    const characters = 'abcdef0123456789';
-    let result = '';
-    for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * characters.length));
+  // Effect to initialize the transaction ID when the component mounts
+  useEffect(() => {
+    if (!witness_tx_id) {
+      setTransactionId(makeHexId(64));
     }
-    return result;
-  }
+  }, [witness_tx_id]);
+
+  const numericValue = parseFloat(value);
+
 
 
   return (
@@ -29,7 +36,7 @@ export const ActivityStoryItem: React.FC<WalletTransaction> = ({ witness_tx_id, 
       <BaseRow gutter={[20, 20]} wrap={true} align="middle">
         <BaseCol span={24}>
           <S.Label>{t('Witness Transaction ID')}:</S.Label>
-          <S.Address style={{ color: 'var(--text-main)' }}>{witness_tx_id ? witness_tx_id : makeHexId(64)}</S.Address>
+          <S.Address style={{ color: 'var(--text-main)' }}>  {witness_tx_id ? witness_tx_id : transactionId}</S.Address>
         </BaseCol>
         <BaseCol span={24}>
           <S.Label>{t('Output')}:</S.Label>
