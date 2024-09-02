58afa6c160a9bdfb444611de290b5c910e3fc989
Maphikza <stapisi@protonmail.com>
2024-09-02 17:05:34 +0200

    Adding ability to send transactions from the panel and adding hook to pull pending transactions

--- src/components/nft-dashboard/Balance/components/SendForm/components/ResultScreen.tsx
+++ src/components/nft-dashboard/Balance/components/SendForm/components/ResultScreen.tsx
@@ -2,27 +2,34 @@ import React from 'react';
 import { BaseButton } from '@app/components/common/BaseButton/BaseButton';
 import { truncateString } from '@app/utils/utils';
 import * as S from './ResultScreen.styles';
+
 interface ResultScreenProps {
   isSuccess: boolean;
   amount: number;
   receiver: string;
+  txid: string;  // New field to display the transaction ID
+  message?: string;  // Optional message from the transaction response
 }
 
-const ResultScreen: React.FC<ResultScreenProps> = ({ isSuccess, amount, receiver }) => {
+const ResultScreen: React.FC<ResultScreenProps> = ({ isSuccess, amount, receiver, txid, message }) => {
   return (
     <S.ResultScreenWrapper>
       {isSuccess ? (
         <>
-          <S.ResultTextPass>Success! </S.ResultTextPass>
+          <S.ResultTextPass>Success!</S.ResultTextPass>
           <S.ResultMessage>
-            {' '}
-            You have successfully sent <br/> {amount} Sats <br/>   to  <br/>  {truncateString(receiver, 10)} 
+            You have successfully sent <br /> {amount} Sats <br /> to <br /> {truncateString(receiver, 10)} 
+            <br />
+            Transaction ID: <br /> {truncateString(txid, 15)}
           </S.ResultMessage>
         </>
       ) : (
         <>
-          <S.ResultTextFail>Failed </S.ResultTextFail>
-          <S.ResultMessage> This transaction was unable to send. </S.ResultMessage>
+          <S.ResultTextFail>Failed</S.ResultTextFail>
+          <S.ResultMessage>
+            This transaction was unable to send. <br />
+            {message && <span>Error: {message}</span>}
+          </S.ResultMessage>
         </>
       )}
     </S.ResultScreenWrapper>
@@ -30,3 +37,4 @@ const ResultScreen: React.FC<ResultScreenProps> = ({ isSuccess, amount, receiver
 };
 
 export default ResultScreen;
+
