484a27c061d8ae62384693423a5c40767629c8cd
TONE-E <90010906+AnthonyMarin@users.noreply.github.com>
2024-09-09 18:57:40 -0700

    added breadcrumb layout

--- src/components/media/MediaManager/MediaManager.styles.ts
+++ src/components/media/MediaManager/MediaManager.styles.ts
@@ -1,18 +1,31 @@
-import styled, {css} from "styled-components";
+import styled from 'styled-components';
+import { BaseButton } from '@app/components/common/BaseButton/BaseButton';
 
 export const MediaManagerContainer = styled.div`
-
-display: flex;
-flex-direction: column;
-gap: 1rem;
-
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `;
 
 export const ToolBarContainer = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-gap: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
 `;
 
-export const MediaItemsContainer = styled.div`  `;  
+export const ButtonsContainer = styled.div`
+  display: flex;
+  gap: 1rem;
+  justify-content: flex-end;
+  width: 100%;
+  padding-right: 1rem;
+`;
+
+export const MediaItemsContainer = styled.div`
+  padding-top: 2rem;
+`;
+
+export const ToolBarButton = styled(BaseButton)`
+  font-size: 0.8rem;
+`
