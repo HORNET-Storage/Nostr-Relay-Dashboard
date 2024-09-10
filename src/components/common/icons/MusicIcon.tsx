64338755315f4ee98cb18cef45ec6b4f8b85cf77
TONE-E <90010906+AnthonyMarin@users.noreply.github.com>
2024-09-10 12:29:07 -0700

    fixed svg warnings

--- src/components/common/icons/MusicIcon.tsx
+++ src/components/common/icons/MusicIcon.tsx
@@ -3,30 +3,30 @@ import React from 'react';
 const MusicIcon: React.FC = () => {
   return (
     <svg width="20" height="20" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clip-path="url(#clip0_1222_39674)">
+      <g clipPath="url(#clip0_1222_39674)">
         <path
           d="M4.35 8.50005C5.09558 8.50005 5.7 7.89563 5.7 7.15005C5.7 6.40446 5.09558 5.80005 4.35 5.80005C3.60442 5.80005 3 6.40446 3 7.15005C3 7.89563 3.60442 8.50005 4.35 8.50005Z"
           stroke="black"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M8.48994 5C8.28479 4.41965 7.90607 3.91645 7.40518 3.55866C6.90429 3.20088 6.30546 3.00584 5.68994 3V7.15"
           stroke="black"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10 0.5H1.5C0.947715 0.5 0.5 0.947715 0.5 1.5V10C0.5 10.5523 0.947715 11 1.5 11H10C10.5523 11 11 10.5523 11 10V1.5C11 0.947715 10.5523 0.5 10 0.5Z"
           stroke="black"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M13.5 3.5V12.5C13.5 12.7652 13.3946 13.0196 13.2071 13.2071C13.0196 13.3946 12.7652 13.5 12.5 13.5H3.5"
           stroke="black"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
--- src/components/media/MediaManager/MediaViewer/MediaViewer.tsx
+++ src/components/media/MediaManager/MediaViewer/MediaViewer.tsx
@@ -12,9 +12,9 @@ const MediaViewer: React.FC<MediaViewerProps> = ({ visible, onClose, file }) => 
   if (!file) return null;
 
   return (
-    <S.Modal open={visible} onCancel={onClose} footer={null} width={800}>
+    <S.Modal  centered={true} open={visible} onCancel={onClose} footer={null} width={800}>
       <S.MediaWrapper>
-        {file.type.startsWith('image') && <img src={file.thumbnail} alt={file.name} style={{ width: '100%' }} />}
+        {file.type.startsWith('image') && <img src={file.thumbnail} alt={file.name} style={{ width: '75%' }} />}
         {file.type.startsWith('video') && <video controls src={file.path} style={{ width: '100%' }} />}
         {file.type.startsWith('audio') && <audio controls src={file.path} style={{ width: '100%' }} />}
         {file.type.startsWith('application') && <S.NotSupported>File type not supported</S.NotSupported>}
