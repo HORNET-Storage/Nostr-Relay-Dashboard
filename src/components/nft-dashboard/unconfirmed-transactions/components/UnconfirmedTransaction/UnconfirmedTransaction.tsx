3200c1486c8a715bb4165453ee3b0e9420de7d2d
TONE-E <90010906+AnthonyMarin@users.noreply.github.com>
2024-09-15 15:16:51 -0700

    added message component

--- src/components/nft-dashboard/unconfirmed-transactions/components/UnconfirmedTransaction/UnconfirmedTransaction.tsx
+++ src/components/nft-dashboard/unconfirmed-transactions/components/UnconfirmedTransaction/UnconfirmedTransaction.tsx
@@ -4,8 +4,8 @@ import { BaseCard } from '@app/components/common/BaseCard/BaseCard';
 import { useResponsive } from '@app/hooks/useResponsive';
 import { truncateString } from '@app/utils/utils';
 import CopyToClipboard from 'react-copy-to-clipboard';
-import { Button } from 'antd';
-import {CopyOutlined } from '@ant-design/icons';
+import { Button, message } from 'antd';
+import { CopyOutlined } from '@ant-design/icons';
 interface UnconfirmedTransactionProps {
   tx_id: string;
   date_created: string;
@@ -15,17 +15,23 @@ interface UnconfirmedTransactionProps {
 
 const UnconfirmedTransaction: React.FC<UnconfirmedTransactionProps> = ({ tx_id, date_created, amount, feeAmount }) => {
   const { isTablet } = useResponsive();
+  const [messageApi, contextHolder] = message.useMessage();
+  const copied = () => {
+    messageApi.open({ content: 'Copied to clipboard', duration: 1, type: 'success' });
+  };
   const onCopy = () => {
     //display Copied to clipboard
-  }
+    copied();
+  };
   return (
     <S.TransactionWrapper>
-      <S.IDWrapper $isMobile= {!isTablet}>
+      {contextHolder}
+      <S.IDWrapper $isMobile={!isTablet}>
         <S.Value>
           {!isTablet ? truncateString(tx_id, 20) : truncateString(tx_id, 35)}
           <S.CopyWrapper>
             <CopyToClipboard text={tx_id}>
-              <Button icon={<CopyOutlined />} size="small" />
+              <Button onClick={onCopy} icon={<CopyOutlined />} size="small" />
             </CopyToClipboard>
           </S.CopyWrapper>
         </S.Value>
